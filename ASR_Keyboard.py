bce57e68bf45374678d0081e9722746bf10a9689
edStorey <estorey90@gmail.com>
2023-04-19 18:59:12 +0100

    made commands chain better took out separate mario detection reduced hops to 5 hops and updated commands txt

--- ASR_Keyboard.py
+++ ASR_Keyboard.py
@@ -12,33 +12,88 @@ def main() :
                 #audio = recognizer.adjust_for_ambient_noise(src)
                 #print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
                 print("Say \"Mario\" to begin:")
-                audio = recognizer.listen(src, timeout=0.1, phrase_time_limit = 1)
+                audio = recognizer.listen(src, timeout=1, phrase_time_limit = 3)
                 speech_to_txt = recognizer.recognize_google(audio).lower()
                 print('\n' + speech_to_txt + '\n')
                 if 'mario' in speech_to_txt :
-                    print("Speak Command:")
-                    audio_comm = recognizer.listen(src, phrase_time_limit = 1.5)
-                    speech_to_comm = recognizer.recognize_google(audio_comm).lower()
-                    print('\nCommand:\n' + speech_to_comm + '\n')
-                    ASR_Keyboard(speech_to_comm)
+                    #print("Speak Command:")
+                    #audio_comm = recognizer.listen(src, phrase_time_limit = 1.5)
+                    #speech_to_comm = recognizer.recognize_google(audio_comm).lower()
+                    #print('\nCommand:\n' + speech_to_comm + '\n')
+                    ASR_Keyboard(speech_to_txt)
                 #release_all() 
             except Exception as ex:
                 print("Sorry. Could not understand.")
 
 
 
-def ASR_Keyboard(command) :
+def ASR_Keyboard(audio) :
     k = PyKeyboard()
     
+    punctuation= '''!()-[];:'"\,<>./?@#$%^&*_~'''
 
     
-    command = command[command.lower().find('mario'):len(command)].split()
+    #command = command[command.lower().find('mario'):len(command)].split()
+    command = audio.split()
+    command = command[1:]
     for i in range(len(command)) : 
         if 'upright' in command[i].lower() :
             command[i] = 'up'
             command.insert(i + 1, 'right')
+        
+        if 'jumprite' in command[i].lower() :
+            command[i] = 'jump'
+            command.insert(i + 1, 'right')
 
+    if 'long' in command  :
+        t = 1
+    elif 'hold' in command :
+        t = -1
+    else :
+        t  = 0.25
+
+    if 'hold' not in command :
+        
+        release_all()
+    else :
+        x = 0
+
+    direction_keys(command, k, t)
+    time.sleep(t)
+    release_all() 
+    
+
+    if 'stop' in command :
+        release_all() 
     for c in command :
+        for x in punctuation:
+            c=c.replace(x,' ')
+        comm_list = c.lower()
+        comm_list = write_right(comm_list)
+        if  comm_list == 'hop' : 
+            for i in range(5) :
+                k.press_key(k.up_key)
+                direction_keys(command, k, 1)
+                
+                time.sleep(1.2)
+                release_all()
+        elif comm_list == 'fire' :
+            k.press_key(k.control_key)
+            time.sleep(0.05)
+            release_all()
+        elif comm_list == 'machine' :
+            for i in range(10) :
+                k.press_key(k.control_key)
+                time.sleep(0.75)
+                release_all()
+        
+def direction_keys(command, k, t) :
+    punctuation= '''!()-[];:'"\,<>./?@#$%^&*_~'''
+    
+    for c in command :
+        
+        for x in punctuation:
+            c=c.replace(x,' ')
         comm_list = c.lower()
         comm_list = write_right(comm_list)
         if comm_list == 'Mario' :
@@ -51,41 +106,11 @@ def ASR_Keyboard(command) :
             k.press_key(k.right_key)
         elif comm_list == 'left' :
             k.press_key(k.left_key)
+            
         elif 'stop' in command :
             release_all() 
-        elif  command[len(command)-1] == 'hop' : 
-            for i in range(20) :
-                k.press_key(k.up_key)
-                time.sleep(0.2)
-                release_all()
-        elif comm_list == 'fire' :
-            k.press_key(k.control_key)
-            time.sleep(0.05)
-            release_all()
-        elif comm_list == 'machine' :
-            for i in range(10) :
-                k.press_key(k.control_key)
-                time.sleep(0.75)
-                release_all()
-            #ASR_Keyboard(command[0:len(command)-1])
 
-    if command[len(command)-1] == 'long' :
-       time.sleep(1)
-       
     
-    
-    else :
-       time.sleep(0.25)
-        
-
-    if 'hold' not in command :
-        release_all()
-    else :
-        x = 0
-
-    if 'stop' in command :
-        release_all() 
-        
 
 
 
@@ -101,12 +126,14 @@ def release_all() :
 
 def write_right(command) :
     command = command.lower()
-    if 'wright' in command  or 'rate' in command or 'rite' in command:
+    if 'wright' in command  or 'rate' in command or 'rite' in command or 'ripe' in command:
         command = 'right'
     elif command == 'lung' or command == 'along':
         command = 'long'
-    elif command == 'hub':
+    elif command == 'hub' or command == 'hot':
         command = 'hop'
+    elif command == 'holt':
+        command = 'hold'
     else :
         command = command
     return command
